6a19187dbc511cc5061857f94385c0eb6967b018
yyujjin <141253669+yyujjin@users.noreply.github.com>
2024-02-10 15:29:19 +0900

    나의 옷장에 옷 정보를 추가하는 API 생성

--- main.go
+++ main.go
@@ -13,29 +13,36 @@ type item struct {
 	Image    string `json:"image"`
 }
 
-var myCloset []*item
+var myCloset []item
+var itemId = 1
 
 func main() {
 	r := gin.Default()
 
 	r.POST("/addToMyCloset", func(c *gin.Context) {
-		var addItem *item
+		var addItem item
+		// item -> itemId 값을 넣어주고, itemID를 1 증가시켜
 		if err := c.BindJSON(&addItem); err != nil {
 			fmt.Println(err)
+			c.JSON(http.StatusBadRequest, gin.H{
+				"status":  "error",
+				"message": "Invalid request. Please provide valid data for clothing registration.",
+			})
 			return
 		}
+
+		addItem.ItemId = itemId
 
 		myCloset = append(myCloset, addItem)
 		fmt.Println(myCloset)
 		c.JSON(http.StatusOK, gin.H{
 			"status":  "success",
 			"message": "Clothing item successfully registered.",
-			"data":    addItem,
+			"data": addItem,
 		})
-		c.JSON(http.StatusBadRequest, gin.H{
-			"status":  "error",
-			"message": "Invalid request. Please provide valid data for clothing registration.",
-		})
+		
+		itemId++
+		
 	})
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
