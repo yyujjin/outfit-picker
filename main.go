34ef9df2b4df52c42d29f0894256b5fef46d3e5f
yyujjin <141253669+yyujjin@users.noreply.github.com>
2024-02-11 16:19:30 +0900

    ✨ DELETE API 생성

--- main.go
+++ main.go
@@ -4,12 +4,13 @@ import (
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"strconv"
 )
 
 type item struct {
 	ItemId   int    `json:"itemId"`
 	ItemName string `json:"itemName"`
-	Category int `json:"category"`
+	Category int    `json:"category"`
 	Image    string `json:"image"`
 }
 
@@ -17,19 +18,18 @@ var myCloset []item
 var itemId = 1
 
 type category struct {
-	id int
+	id   int
 	name string
-}  
-
-var categoryList = []category{
-	{1,"아우터"},
-	{2,"상의"},
-	{3,"하의"},
-	{4,"신발"},
-	{5,"가방"},
-	{6,"악세서리"},
 }
 
+var categoryList = []category{
+	{1, "아우터"},
+	{2, "상의"},
+	{3, "하의"},
+	{4, "신발"},
+	{5, "가방"},
+	{6, "악세서리"},
+}
 
 func main() {
 	r := gin.Default()
@@ -53,18 +53,41 @@ func main() {
 		c.JSON(http.StatusOK, gin.H{
 			"status":  "success",
 			"message": "Clothing item successfully registered.",
-			"data": addItem,
+			"data":    addItem,
 		})
 
 		itemId++
-		
 	})
 
 	r.GET("/myCloset", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
+		c.IndentedJSON(http.StatusOK, gin.H{
 			"data": myCloset,
+		})
+	})
+
+	r.DELETE("/removeItem/:id", func(c *gin.Context) {
+		id, err := strconv.Atoi(c.Param("id"))
+		fmt.Println(id, err)
+		if err != nil {
+			fmt.Println("경고")
+			c.IndentedJSON(http.StatusNotFound, gin.H{"status": "error",
+				"message": "Invalid request. Please provide valid data for clothing registration.",
+			})
+			return
+		}
+		for index, item := range myCloset {
+			if item.ItemId == id {
+				myCloset = append(myCloset[:index], myCloset[index+1:]...)
+				c.IndentedJSON(http.StatusOK, item)
+				return
+			}
+		}
+		c.JSON(http.StatusNotFound, gin.H{
+			"data": "id를 찾을 수 없습니다.",
 		})
 	})
 
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
+
+//옷장 아이템 삭제, 조회 API 만들기
