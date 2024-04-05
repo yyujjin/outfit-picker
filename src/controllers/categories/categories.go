78335d2ba291a3d77f3344d9e67ca144fd9220c7
yyujjin <141253669+yyujjin@users.noreply.github.com>
2024-04-06 01:42:13 +0900

    Update categories.go

--- src/controllers/categories/categories.go
+++ src/controllers/categories/categories.go
@@ -1,8 +1,15 @@
 package categories
 
 import (
+	"database/sql"
+	"fmt"
+	"log"
 	"net/http"
+<<<<<<< HEAD
 	"outfit-picker/src/models/categorydb"
+=======
+	"os"
+>>>>>>> parent of a4f4a86 (categories sql 모델 생성)
 
 	"github.com/gin-gonic/gin"
 )
@@ -10,9 +17,42 @@ import (
 
 func GetCategories(c *gin.Context) {
 
-	categoryList := categorydb.GetCategoryList()
+	password := os.Getenv("DB_password")
+	fmt.Println(password)
+	dataSourceName := fmt.Sprintf("root:%s@tcp(127.0.0.1:3306)/outfit-picker", password)
 
-	c.JSON(http.StatusOK, gin.H{
+	db, err := sql.Open("mysql", dataSourceName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+
+	type category struct {
+		Id   int
+		Name string
+	}
+
+	var id int
+	var name string
+
+	rows, err := db.Query("SELECT * FROM categorylist ORDER BY id ASC;")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close() //반드시 닫는다 (지연하여 닫기)
+	categoryList := []category{}
+
+	for rows.Next() {
+		err := rows.Scan(&id, &name)
+		if err != nil {
+			log.Fatal(err)
+		}
+		categoryList = append(categoryList, category{id, name})
+		fmt.Println(id, name)
+	}
+
+	c.IndentedJSON(http.StatusOK, gin.H{
 		"data": categoryList,
 	})
 }
