e095c61096f062bc1baad0376825c4dcef6aed45
yyujjin <141253669+yyujjin@users.noreply.github.com>
2024-04-13 02:04:39 +0900

    ♻️ GET API GORM 적용

--- src/controllers/categories/categories.go
+++ src/controllers/categories/categories.go
@@ -1,8 +1,6 @@
 package categories
 
 import (
-	"fmt"
-	"log"
 	"net/http"
 	"outfit-picker/src/models/categorydb"
 
@@ -11,33 +9,8 @@ import (
 
 func GetCategories(c *gin.Context) {
 
-	type category struct {
-		Id   int
-		Name string
-	}
-
-	rows,err := categorydb.SelectCategories()
-
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer rows.Close() 
-
-	categoryList := []category{}
-
-	for rows.Next() {
-
-		var id int
-		var name string
-
-		err := rows.Scan(&id, &name)
-		if err != nil {
-			log.Fatal(err)
-		}
-		categoryList = append(categoryList, category{id, name})
-		fmt.Println(id, name)
-	}
-
+	categoryList := categorydb.SelectCategories()
+	
 	c.IndentedJSON(http.StatusOK, gin.H{
 		"data": categoryList,
 	})
